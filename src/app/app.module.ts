cbd04d0d236fb72d1cc4a5a23a4ae0ee19c52b07
Mike Hartington <mikehartington@gmail.com>
2019-12-18 13:48:56 -0500

    add beta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { SplashScreen } from '@ionic-native/splash-screen/ngx'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ SplashScreen ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
